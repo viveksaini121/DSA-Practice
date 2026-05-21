d5860aa1e10ef194d93c93a027ee8a96fd8c4b03
viveksaini121 <sainivivekk@gmail.com>
2026-05-21 13:53:30 +0530

    Pattern 11

---
+++ pattern11.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    for(int i=0; i <5; i++){
+        for(int j=0; j<=i; j++){
+            if(i%2==j%2)
+                cout << "1";
+            else
+                cout <<"0";
+            }
+
+                cout << endl;
+        }
+    }
