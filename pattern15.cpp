8b320cd7766ef23f41557464a34f5f2b8a8b26fd
viveksaini121 <sainivivekk@gmail.com>
2026-05-22 23:56:17 +0530

    pattern 15

---
+++ pattern15.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    for (int i =5 ; i>0;i--){
+        for(char j='A' ; j<='A'+i;j++){
+            cout << j;
+
+        }
+        cout << endl;
+    }
+}
