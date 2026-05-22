d862e848365d74e7f33f35c55c7bd623ebb01726
viveksaini121 <sainivivekk@gmail.com>
2026-05-22 22:51:46 +0530

    till pattern 14

---
+++ pattern13.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n';
+
+int main(){
+    int k=1;
+    for(int i=0; i<5;i++){
+          for(int j=0; j<=i; j++){
+            cout << k+i << " ";
+          }
+          cout << endl;
+    }
+}
