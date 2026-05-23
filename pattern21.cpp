6c35ba25b45ac410817d54d330a0c429392f635c
viveksaini121 <sainivivekk@gmail.com>
2026-05-23 12:43:24 +0530

    if there's another approach?

---
+++ pattern21.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    for(int i=0; i<4;  i++){
+        for(int j=0; j<4; j++){
+            if(i==0 || i==4-1){
+                cout << "*";
+            }
+            else{
+                if( j==0 || j==4-1){
+                    cout << "*";
+                }
+                else{
+                    cout <<" ";
+                }
+            }
+        }
+            cout << endl;
+        }
+    }
