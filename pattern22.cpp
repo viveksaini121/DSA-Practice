83dabd82efcdc741820fb81b66bcdec030332e09
viveksaini121 <sainivivekk@gmail.com>
2026-05-23 19:29:01 +0530

    PATTERNS DONE.

---
+++ pattern22.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    for(int i=0; i<7; i++){
+        for(int j=0; j<7; j++){
+            cout << (7+1)/2;
+        }
+        cout << endl;
+    }
+// incomplete
+
+}
