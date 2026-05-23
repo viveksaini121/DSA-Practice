aca7ed8a5579f28c3e83d82b2d11784589a12a4c
viveksaini121 <sainivivekk@gmail.com>
2026-05-23 12:04:53 +0530

    pattern 19

---
+++ pattern19.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    for(int i =0; i<5; i++){
+        for(int k=5; k>i; k--){
+            cout<<"★";
+        }
+        for(int j=0; j<2*i; j++){
+            cout<<" ";
+        }
+        for(int k=5; k>i; k--){
+            cout<<"★";
+        }
+        cout << endl;
+    }
+    for(int i =5; i>0; i--){
+        for(int k=5; k>=i; k--){
+            cout<<"★";
+        }
+        for(int j=0; j<2*(i-1); j++){
+            cout<<" ";
+        }
+        for(int k=5; k>=i; k--){
+            cout<<"★";
+        }
+        cout << endl;
+    }
+}
